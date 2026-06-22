535beade51df1413d0d936eed51022a0f60f3c89
osama <osama@example.com>
2026-06-22 05:30:23 -0700

    ggggg

---
+++ learning/homework.py
@@ -0,0 +1,3 @@
+text = "Football is the most popular sport. It unites billions of fans worldwide. This beautiful game showcases raw talent. Professional players train relentlessly for glory. Passionate supporters fill massive stadiums. Tournaments like the World Cup bring people together. The sport is a massive global industry. It creates international football legends. Football is a true way of life. It teaches teamwork and fair play."
+words = text.split()
+print(f"numbers of words is : {len(words)} ")
