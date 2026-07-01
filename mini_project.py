ebf1c11e3bf9bafdf08a368bf4469b46dca76184
osama <osama@example.com>
2026-07-01 03:17:53 -0700

    hhhh

---
+++ mini_project.py
@@ -0,0 +1,29 @@
+# library : view / add / delete / search 
+
+# list of library 
+library = [
+    {"Tital" : 'white night' ,
+     "Author" : 'destoyfiske'} ,
+    {"Tital" : 'The end' , 
+     "Author" : 'osama'}
+]
+
+# Add 
+
+
+
+# Delete 
+
+
+
+
+
+# to check is found or not founded 
+
+
+
+
+# Search 
+
+
+
