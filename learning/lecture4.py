2dacdad3d2b9f6c9afe24ea357610e3a81769bc9
osama <osama@example.com>
2026-07-01 14:02:46 -0700

    aaa

--- learning/lecture4.py
+++ learning/lecture4.py
@@ -1,7 +1,7 @@
 l =[]
 
 while True :
-    x = input("Enter a num or type q to exit")
+    x = input("Enter a num or type q to exit : ")
     if x.lower() == "q":
         break
     x = float(x)
@@ -9,6 +9,10 @@ while True :
 print(l)    
 
 def average(li) :
+
+    if len(li) == 0:    # إذا كانت القائمة فارغة، len(li) ستكون 0
+        return 0 
+
     sum = 0
     for i in li :
         sum += i
@@ -20,13 +24,18 @@ print(average(l))
 
 # part 2
 
-def average1(*li):
+def average1(*li):   # we use The star to acces to the Tuple  يعني مش بحاجه ل ليست عشان احط الارقام 
+
+    if len(li) == 0:    # إذا كانت القائمة فارغة، len(li) ستكون 0
+        return 0 
+    
     sum = 0 
     for i in li :
         sum += i
     avg = sum    /len(li)
     return avg 
 print(average1(1,2,3,4,5))
+print(average1(*l))       # باستعمال ال * قدرت استعمل اللست بس بدونها راح يوخذ الليست كقيمه وحده مش عده قيم وراح يكون غلط
 
 # part 3 (factorial)
 x = 5
