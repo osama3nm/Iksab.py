34db221148f25b6200c47f8563e37b4a63c4503d
osama <osama@example.com>
2026-07-02 14:00:58 -0700

    hhhh

--- learning/BankAccount.py
+++ learning/BankAccount.py
@@ -1,5 +1,6 @@
 class BankAccount :
     customers = 0
+    total_savings = 0        # اجمالي المدخرات 
     def __init__(self,id=2024255465,balance=0,iban=5478-8569-5847-9658):      # iban : number of bank account like(5478-8569-5847-9658)
         self.__id = id
         self.__balance = balance         
@@ -13,6 +14,9 @@ class BankAccount :
     
     def set_balance(self,balance):
         self.__balance += balance 
+        BankAccount.total_savings += balance
+
+
     def get_balance(self):
         return self.__balance
     
@@ -21,15 +25,24 @@ class BankAccount :
     def get_iban(self):
         return self.__iban
     
-
+# customer 1
 customer1 = BankAccount(balance=100)     # بزبط احدد متغير واحد واوديلو قيمو من خلال انو احط اسم هاض المتغير 
 print(customer1.get_id())
 print(customer1.get_balance())
 customer1.set_balance(-20)
-print(customer1.get_balance())
+print(f"totally balance of customer 1 : {customer1.get_balance()}")
+
+
+# customer 2
+customer2 = BankAccount(2044568,100,2556-9665-5447-7885)
+customer2.set_balance(1000)
+customer2.set_balance(-180)
+customer2.set_balance(55)
+print(f"totally balance of customer 2 : {customer2.get_balance()}")
 
 
 
 print(f"number of Customers is : {BankAccount.customers}")
+print(f"Total savings is : {BankAccount.total_savings}")
 
 
