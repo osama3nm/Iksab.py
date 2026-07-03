733a1281dd171cc6bc044470a2e4578727226768
osama <osama@example.com>
2026-07-03 09:00:09 -0700

    ddd

--- learning/BankAccount.py
+++ learning/BankAccount.py
@@ -13,12 +13,31 @@ class BankAccount :
     def get_id(self):
         return self.__id
     
-    def set_balance(self,balance):
-        self.__balance += balance 
-        BankAccount.total_savings += balance
+    def deposit(self,balance):        # ايداع
+        if balance <= 0 :
+            print("ما بزبط اودع بالحساب صفر او اقل من صفر ❎")
+            return True
+        else :
+            self.__balance += balance 
+            BankAccount.total_savings += balance
+            return True
+
+    def withdraw(self,balance):       # سحب 
+        if balance > self.__balance :
+            print("الرصيد اقل من عمليه سحبك")
+            return False 
+        elif balance <= 0 :
+            print("لازم تسحب مصاري بزبطش تسحب صفر او قيمه سالبه")
+            return False
+        else :
+            print("تم السحب بنجاح ✅")  
+            self.__balance -= balance  
+            BankAccount.total_savings -= balance
+            return True
 
 
-    def get_balance(self):
+
+    def display_balance(self):
         return self.__balance
     
     def set_iban(self,iban):
@@ -29,17 +48,17 @@ class BankAccount :
 # customer 1
 customer1 = BankAccount(balance=100)     # بزبط احدد متغير واحد واوديلو قيمو من خلال انو احط اسم هاض المتغير 
 print(customer1.get_id())
-print(customer1.get_balance())
-customer1.set_balance(-20)
-print(f"totally balance of customer 1 : {customer1.get_balance()}")
+print(customer1.display_balance())
+customer1.deposit(-20)
+print(f"totally balance of customer 1 : {customer1.display_balance()}")
 
 
 # customer 2
 customer2 = BankAccount(2044568,100,2556-9665-5447-7885)
-customer2.set_balance(1000)
-customer2.set_balance(-180)
-customer2.set_balance(55)
-print(f"totally balance of customer 2 : {customer2.get_balance()}")
+customer2.deposit(1000)
+customer2.deposit(-180)
+customer2.deposit(55)
+print(f"totally balance of customer 2 : {customer2.display_balance()}")
 
 
 
